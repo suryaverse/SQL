6f02d848762d04355fd946c41d84e45817f821d7
surya <suryasomu066@gmail.com>
2022-08-17 15:21:44 +0530

    1st sql program

--- src/sql.java
+++ src/sql.java
@@ -1,27 +1,29 @@
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class sql {
     public static void main(String[] args) throws Exception {
         String jdbcURL = "jdbc:postgresql://localhost:5432/surya";
         String username = "surya";
         String password = "6667";
+        Statement statement = null;
         try {
+            Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(jdbcURL, username, password);
-            String sql = "INSERT INTO student (name,roll_no,dob,dept_name)"
+            String sql = "INSERT INTO college.student (name,roll_no,dob,dept_name)"
                     + "VALUES('naina','19','1998/05/18','ECE')";
-            Statement statement = (Statement) connection.createStatement();
-            int rows = ((java.sql.Statement) statement).executeUpdate(sql);
+            statement = connection.createStatement();
+            int rows = statement.executeUpdate(sql);
             if (rows > 0) {
                 System.out.println("Created");
             }
             System.out.println("Connected");
             connection.close();
         } catch (SQLException e) {
-            System.out.println("Error");
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
         }
-
     }
 }
